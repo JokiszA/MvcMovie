6e4b82e7eab9ea3d31c30394496b1d9b5a23f938
jokisza <adrian.jokisz@gmail.com>
2025-03-27 16:56:03 +0100

    new field

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b07cdc4475a368d1682f1973863538e60ae11ab9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+027a9a5becf7ad994c74bd2f4eb8fcec750b0b34")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
